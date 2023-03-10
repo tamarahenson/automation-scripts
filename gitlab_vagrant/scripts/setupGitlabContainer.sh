30baba16c14e40195192fa5876915bd451c12337
Tamara Henson <henson.tamara@gmail.com>
2023-03-10 11:49:34 -0800

    Create setupGitlabContainer.sh

---
+++ gitlab_vagrant/scripts/setupGitlabContainer.sh
@@ -0,0 +1,10 @@
+  docker run --detach \
+  --hostname gitlab.example.com \
+  --publish 443:443 --publish 80:80 \
+  --name gitlab \
+  --restart always \
+  --volume $GITLAB_HOME/config:/etc/gitlab \
+  --volume $GITLAB_HOME/logs:/var/log/gitlab \
+  --volume $GITLAB_HOME/data:/var/opt/gitlab \
+  --shm-size 256m \
+  gitlab/gitlab-ce:latest
