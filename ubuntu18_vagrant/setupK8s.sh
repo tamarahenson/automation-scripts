d63d0bd8acc36b776266843c082a1596a739da3a
Tamara Henson <henson.tamara@gmail.com>
2025-04-26 17:58:37 -0700

    Update K8s Script
    Adds additional utilities:  helm and  k9s

--- ubuntu18_vagrant/setupK8s.sh
+++ ubuntu18_vagrant/setupK8s.sh
@@ -1,1 +1,16 @@
-curl https://kurl.sh/9962f98 | sudo bash
+#!/usr/bin/env bash
+
+echo "Installing K8s Cluster"
+
+curl https://kurl.sh/4b5bb56 | sudo bash
+
+echo "Installing Helm"
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+echo "Installing K9s"
+
+wget https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_linux_amd64.deb
+sudo apt install ./k9s_linux_amd64.deb
