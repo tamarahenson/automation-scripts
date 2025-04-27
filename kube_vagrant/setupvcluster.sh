3a6603303ae235252361e9ff81f5f5bde7053d66
Tamara Henson <henson.tamara@gmail.com>
2025-04-26 18:11:34 -0700

    Add vcluster
    installs vcluster

---
+++ kube_vagrant/setupvcluster.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "Installing K8s Cluster"
+
+curl https://kurl.sh/4b5bb56 | sudo bash
+
+echo "Installing Helm"
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+echo "Installing K9s"
+
+wget https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_linux_amd64.deb
+sudo apt install ./k9s_linux_amd64.deb
+
+echo "Installing VCluster"
+
+curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64" && sudo install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
